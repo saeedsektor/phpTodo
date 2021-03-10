7f48b0b21290508c1a87abd3c3b94fab1906480f
skdev <saeedsektor@gmail.com>
2021-03-10 09:32:58 -0800

    tiny changes in comments

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,9 +1,4 @@
 <?php
-  /*
-   * App Core Class
-   * Creates URL & loads core controller
-   * URL FORMAT - /controller/method/params
-   */
   class Core {
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
